7fe4ea66537069e1645a6a9ad80a470284f5a19e
Yukari Hori <y.hori@utoronto.ca>
2025-03-04 14:28:43 -0700

    Change WelcomeBanner text

--- ui/src/components/Home/WelcomeBanner/WelcomeBanner.js
+++ ui/src/components/Home/WelcomeBanner/WelcomeBanner.js
@@ -12,15 +12,15 @@ const WelcomeBanner = () =>{
         <Row>
           <Col xs lg="4" className="text-center my-auto">
             <h2 className="welcomeTitle">
-              Welcome to the Winter Road and Trail Data Information Portal
+              Welcome to the Winter Road and Mobility Portal
             </h2>
             <p className="welcomeText">
-              The Winter Road and Trail Data Information Portal (WRTDIP)
+              The Winter Road and Mobility Portal (WRaMP)
               provides an interactive map with northern climate data,
-              transportation networks, and social aspects of the winter road and
-              trail networks in Canada's Territorial North. The aim of this
+              transportation networks, and social aspects of the winter road
+              networks in Canada's Territorial North. The aim of this
               portal is to support decision-making and policy prioritization on
-              the management of winter roads and trails that are impacted by
+              the management of winter roads that are impacted by
               climate change.
             </p>
           </Col>
