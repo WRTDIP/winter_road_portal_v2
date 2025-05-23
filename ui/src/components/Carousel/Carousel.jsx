42e4d5057576667f71417acf026010aed9acbcff
Yukari Hori <y.hori@utoronto.ca>
2025-05-23 11:24:12 -0400

    Chaange Home pictures

--- ui/src/components/Carousel/Carousel.jsx
+++ ui/src/components/Carousel/Carousel.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import homebg_1 from "../../assets/homebg_1.png"
 import homebg_2 from "../../assets/homebg_2.png";
-import homebg_3 from "../../assets/homebg_3.png";
+import homebg_3 from "../../assets/homebg_3.1.png";
+import homebg_4 from "../../assets/homebg_4.png";
 import './styles.css';
 
 function CarouselHome() {
@@ -17,6 +18,9 @@ function CarouselHome() {
         </Carousel.Item>
         <Carousel.Item>
           <img className="carousel-image" src={homebg_3} alt="Third slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="carousel-image" src={homebg_4} alt="Fourth slide" />
         </Carousel.Item>
       </Carousel>
     </div>
