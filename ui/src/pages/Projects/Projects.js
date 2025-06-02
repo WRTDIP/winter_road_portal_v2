2a3e46f1a34d6cc4d8fee72f52a7b8bd0142633d
Yukari Hori <y.hori@utoronto.ca>
2025-06-02 16:07:14 -0400

    Re-added PDF files at Project page

--- ui/src/pages/Projects/Projects.js
+++ ui/src/pages/Projects/Projects.js
@@ -4,8 +4,9 @@ import ProjectSecond from "../../assets/online_survey_icon.png";
 import ProjectThird from "../../assets/ice_road_icon.png";
 import CoverBanner from "../../components/Global/CoverBanner/CoverBanner";
 import ProjectItem from "../../components/Projects/ProjectBanner/ProjectItem";
-import WRWatchInstruction from "../../assets/2025-05_WR_Watch_Instruction.pdf";
-import WRWatchQR from "../../assets/2025-05_WR_Watch_QR_Code.pdf";
+import WRWatchInstruction from "../../assets/2025-06_Winter_Road_Watch_Instruction.pdf";
+import WRWatchQR from "../../assets/2025-06_Winter_Road_Watch_QR_Code.pdf";
+import WRWatchWorkflows from "../../assets/2025-06_Winter_Road_Watch_Data_Collection_Workflows.pdf";
 
 function Projects() {
   const BannerContents = [
@@ -21,14 +22,21 @@ function Projects() {
           <br />
           <span style={{ fontSize: "1.5rem", fontWeight: "bold" }}>Getting Started</span>
           <br />
-          To participate, please read the step-by-step instructions for the Winter Road Watch App on how to set up and contribute your observations.
+          To participate, please read the step-by-step instructions for the Winter Road Watch Survey123 App on how to set up and contribute your observations.
           <br />
           <a href={WRWatchInstruction} target="_blank" rel="noopener noreferrer">
             Download the Winter Road Watch Survey123 App Instruction
           </a>
           <br />
           <br />
-          To start the Winter Road Watch App on your mobile device, please scan the QR code. Also, scan the code to view observations from others on the mobile version of the interactive web map.
+          To view the workflows for the Winter Road Watch, please download the Winter Road Watch Data Collection Workflows document.
+          <br />
+          <a href={WRWatchWorkflows} target="_blank" rel="noopener noreferrer">
+            Download the Winter Road Watch Data Collection Workflows
+          </a>
+          <br />
+          <br />
+          To download the Winter Road Watch survey on your mobile device, please scan the QR code. Also, scan the code to view observations from others on the mobile version of the interactive web map.
           <br />
           <span>
           <strong>Note: We recommend viewing the interactive web map on a larger screen (e.g., computer or tablet). 
@@ -49,7 +57,7 @@ function Projects() {
       image: ProjectSecond,
       title: "Winter Road User Community Online Survey",
       content:
-        "We will invite the Winter Road Watch observers to participate in an online survey focused on the impacts of climate change on the winter road networks in the Northwest Territories.",
+        "We invite Winter Road Watch observers to participate in an online survey focused on the impacts of climate change on the winter road networks in the Northwest Territories.",
       bgColor: "#F7FAEF",
     },
     {
