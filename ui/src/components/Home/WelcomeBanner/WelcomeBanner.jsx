12f386f4acf15bac8c9180ec11b621ed72988768
Mohammad Rahman <152694468+mohammadrahman3@users.noreply.github.com>
2026-08-20 11:37:41 -0400

    Refactor WelcomeBanner component structure

--- ui/src/components/Home/WelcomeBanner/WelcomeBanner.jsx
+++ ui/src/components/Home/WelcomeBanner/WelcomeBanner.jsx
@@ -12,7 +12,7 @@ const WelcomeBanner = () => {
       <Row>
         <Col xs lg="4" className="text-center my-auto">
           <h2 className="welcomeTitle">
-            Welcome to WRaMP Test <br />
+            Welcome to WRaMP <br />
             Winter Road and Mobility Portal
           </h2>
           <p className="welcomeText">
