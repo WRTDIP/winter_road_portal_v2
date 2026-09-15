95e363e9fac851defea29882a06bfdab949c04ae
Mohammad Rahman <mohammadk.rahman@mail.utoronto.ca>
2026-09-15 15:52:22 +0000

    Remove duplicate antd.variable.min.css theme import
    The variable theme loaded after the base theme, overriding selectors and shifting
    colors, borders, and padding across all 16 routes. Removing it is semantically
    correct (only one theme should load) and lets components use their intended values.

--- ui/src/index.jsx
+++ ui/src/index.jsx
@@ -4,7 +4,6 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "antd/dist/antd.min.css";
-import "antd/dist/antd.variable.min.css";
 import NavigationBar from "./components/Global/NavigationBar/index.jsx";
 import Home from "./pages/Home/Home.jsx";
 import Footer from "./components/Global/Footer/index.jsx";
